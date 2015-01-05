d6be28c0c0ba7d565c58af44ecad11e228a4c237
acarvalh <alexandra.oliveira@cern.ch>
2015-01-05 15:43:01 +0100

    teste

--- Functions.h
+++ Functions.h
@@ -1,4 +1,4 @@
-// Test function
+// Test function //
 void hello();
 //////////////////////////////////////////////////////////
 // histos
@@ -10,10 +10,13 @@ int truetops(vector<PseudoJet> jets, vector<PseudoJet> leptons,vector<PseudoJet>
 // tags
 int recojets(vector<PseudoJet> particles,vector<PseudoJet> & jets, vector<int> & btag, 
              vector<int> & bmistag, vector<int> & fattag, vector<int> & btrue);
-bool recol(double & met, vector<PseudoJet> jets,vector<PseudoJet> leptons,vector<PseudoJet> neutrinos);
+int recol(vector<PseudoJet> jets,vector<PseudoJet> leptons,vector<PseudoJet> neutrinos);
 bool recohadt(int & bh, int & bl, vector<PseudoJet> jets,vector<PseudoJet> leptons,vector<PseudoJet> neutrinos, 
               vector<int> btag, vector<int> btrue, double met);
 bool recolept2step(int & bh, int & bl,vector<PseudoJet> jets, vector<PseudoJet> leptons,vector<PseudoJet> neutrinos, 
               vector<int> btag, vector<int> btrue, double met);
 bool recotlepeq(int & bh, int & bl,vector<PseudoJet> jets, vector<PseudoJet> leptons,vector<PseudoJet> neutrinos, 
+              vector<int> btag, vector<int> btrue, double met);
+
+bool fullylep(int & bh, int & bl, vector<PseudoJet> jets,vector<PseudoJet> leptons,vector<PseudoJet> neutrinos, 
               vector<int> btag, vector<int> btrue, double met);
